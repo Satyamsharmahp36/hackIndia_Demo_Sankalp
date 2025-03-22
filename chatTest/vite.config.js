245d0a1668b7adf1b0097cd136a36a01be9d75ba
Satyam <satyamworks20@gmail.com>
2025-03-22 19:54:56 +0530

    Some deployment chnages added

--- chatTest/vite.config.js
+++ chatTest/vite.config.js
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-})
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist", // Ensures correct build directory
+    assetsDir: "assets", // Stores assets properly
+  },
+  server: {
+    mimeTypes: {
+      "application/javascript": ["js"],
+    },
+  },
+  publicDir: "public", // Ensures `_headers` is included
+});
